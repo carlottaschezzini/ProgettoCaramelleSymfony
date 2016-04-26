9d4e6bc8a731e399d87fb6ccf5660689c19ffb19
antoniogaldo <antoniohendrix@hotmail.it>
2016-04-26 17:05:55 +0200

    form funzionante

--- src/AdminBundle/Form/Type/AulaFormType.php
+++ src/AdminBundle/Form/Type/AulaFormType.php
@@ -4,6 +4,7 @@ namespace AdminBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
@@ -15,6 +16,7 @@ class AulaFormType extends AbstractType
             ->add('nome', TextType::class, array('label' => false))
             ->add('sede', EntityType::class, array('class' => 'UserBundle:Sede', 'choice_label' => 'citta'))
             ->add('citta', TextType::class, array('label' => false))
+            ->add('posti', IntegerType::class, array('label' => false))
            
             ->add('save', SubmitType::class, array('label' => false))
         ;
